7788cbf36ea94303a8c2a244860d647f76570669
flaoua <stephane.trebel@gmail.com>
2016-05-03 08:24:20 +0200

    Fix npm Test scripts (both local and CI)

--- tests/unit/lib/middlewares/index.js
+++ tests/unit/lib/middlewares/index.js
@@ -5,31 +5,28 @@ const m = require("../../../../lib/middlewares/index");
 const nullFn = () => null;
 const lodash = require("lodash/fp");
 
-test("Middleware manager", (t) => {
-    test("getPort() ", (t) => {
-        t.equal(typeof m.getPort, "function", "should be a function");
+test("Middleware manager", function (t) {
+    test("getPort() ", function (t) {
         t.throws(() => m.getPort(), "should throw when given an invalid context");
         t.equal(m.getPort({
-                get: (a, b) => b[a]
-            }, {
-                port: 1
-            }), 1,
-            "should get the config port when given valid context");
-        t.end();
-    });
-    test("mwInit()", (t) => {
-        t.equal(typeof m.mwInit, "function", "should be a function");
-        t.end();
-    });
-    test("mwInit()", (t) =>
-        t.shouldFail(m.mwInit(), TypeError, "should reject empty calls"));
-    test("mwInit()", (t) =>
-        m.mwInit(lodash.defaults({}, lodash, { requireModule: a => a }), {
-            use: nullFn
+            get: (a, b) => b[a]
         }, {
-            middlewareList: [() => 'A', () => 'B']
-        }, nullFn)
-        .then((result) => t.equal(result, true, "should initialize middlewares"))
-    );
+            port: 1
+        }), 1,
+                "should get the config port when given valid context");
+        t.end();
+    });
+    test("mwInit()", function (t) {
+        t.shouldFail(m.mwInit(), TypeError, "should reject empty calls");
+    });
+    test("mwInit()", function (t) {
+        return m
+            .mwInit(lodash.defaults({}, lodash, {requireModule: (a) => a}), {
+                use: nullFn
+            }, {
+                middlewareList: [() => 'A', () => 'B']
+            }, nullFn)
+            .then((result) => t.equal(result, true, "should initialize middlewares"));
+    });
     t.end();
 });
