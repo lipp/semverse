a04ebec63ac089c171e99ea993ec6b1a176ed05d
flaoua <stephane.trebel@gmail.com>
2016-06-02 18:00:04 +0200

    Update config module to be just an empty object for the moment

--- src/config/index.spec.js
+++ src/config/index.spec.js
@@ -3,36 +3,21 @@
 const path = require("path");
 
 const {
-    // Primary test functions
-    testModuleBlock,
-    prepareStubs,
-
-    // Helpers
-    nullFn,
-    nullFnHO,
-    throwFn,
-    throwFnHO,
-    resolveFn,
-
-    // Dependencies to stub
-    express,
-    config,
-    utils,
-    middlewares
+    t,
+    prepareStubs
 } = require(path.resolve("src/lib/test-helpers"));
 
 const m = prepareStubs(path.resolve(__dirname, "./index"));
 
-testModuleBlock("Config loader", (testBlock) => {
-    testBlock("getService()", (unitTest) => {
-        unitTest("since it's not implemented yet", (t) => {
-            t.notEqual(
-                typeof m({
-                    [utils]: { tbd: () => null }
-                }).getService,
-                undefined,
-                "should be defined");
+t("Config loader", function(t) {
+    //t("getService()", function(t) {
+        t.test("since it's not implemented yet", function(t) {
+            t.deepEqual(
+                m({
+                }),
+                {},
+                "should be defined as an empty object");
             t.end();
         });
-    });
+    //});
 });
