1c6e3b1b35e5fe93a05b417521e14bdfbf49d8c9
Stéphane Trebel <stephane.trebel@gmail.com>
2016-06-02 19:34:14 +0200

    Basic framework (#3)
    * Update Travis-CI test script
    
    * Update npm test sequence
    
    * Refactor service startup and crash log
    
    * Update npm test worflow (simpler)
    
    * Factor 'tbd' function into lib/utils
    
    * Update Travis-CI script
    
    * Update npm test scripts (get rid of tap.log temp file)
    
    * Refactor tests to replace stubMe with proxyquire noCallThru configuration
    
    * Add JSDoc engine
    
    * Add content to README
    
    * Change doc/ to docs/ and ignore it in coverage
    
    * Update test harness
    
    * Add fudge option in JSLint configuration file
    
    * Add docs for version 2.0.0
    
    * Update version number from 1.0.0 to 2.0.0 and Refactor test scripts
    
    * Update config module to be just an empty object for the moment
    
    * Update service main function to properly load middlewares
    
    * Rewrite middleware loader module
    
    * Rewrite test helpers to hide almost all harness logic
    
    * Rewrite utils with only one function for the moment: requireFromProjectRoot
    
    * Add JSLint dependency
    
    * Add Blue-Tape and Nodemon dependencies
    
    * Quickfix

--- src/lib/test-helpers.js
+++ src/lib/test-helpers.js
@@ -2,58 +2,71 @@
 
 const path = require("path");
 
-const lodash = require("lodash");
-const test = require("tape");
-const proxyquire = require("proxyquire");
+const lodash = require("lodash/fp");
+const BPromise = require("bluebird");
 
-// Export blue-tape Test function
+const tape = require("blue-tape");
+const sinon = require("sinon");
+const proxyquire = require("proxyquire").noPreserveCache().noCallThru();
+
 /**
- * Returns a curried function that will execute a test with a merged
- * description
- * @param {String} blockDescription Test block description
- * @param {String} testDescription Test description
- * @param {Function} cb Callback call with blue-tape 't' object
- * @return {CurriedFunction} Unit test function
+ * Returns a curried function that will execute a test with a description
+ * pre fed by the parent test block
+ * @curried
+ * @private
+ * @param {Function} testFn - Test engine
+ * @param {String} blockDescription - Parent block description
+ * @param {String} testDescription - This Test description
+ * @param {Function} cb - Callback called with test engine object
+ * @return {Function} Atomic test handler
  */
 exports.unitTest = lodash.curry(
-    function unitTest(blockDescription, testDescription, cb) {
-        return test(`${blockDescription} : ${testDescription}`, cb);
+    function unitTest(testFn, blockDescription, testDescription, cb) {
+        return testFn(`${blockDescription} : ${testDescription}`, cb);
     });
 
 /**
- * Returns a curried function that will execute several tests with a merged
+ * Returns a function that will execute several tests with a concatenated
  * description
- * @param {String} moduleDescription Module description
- * @param {String} blockDescription Test block description
- * @return {CurriedFunction} Test block function
+ * @public
+ * @param {String} parentBlockDescription - Parent block description
+ * @param {String} [blockDescription] - This test block description
+ * @param {Function} cb - Callback
+ * @return {Function} Test block handler
  */
-exports.testBlock = lodash.curry(
-    function testBlock(moduleDescription, blockDescription, cb) {
-        return cb(
-            exports.unitTest(`${moduleDescription} - ${blockDescription}`)
-        );
-    });
+exports.t = function t(parentBlockDescription, blockDescription, cb) {
+    let cb2;
+    let msg;
+    if (lodash.isFunction(blockDescription)) {
+        cb2 = blockDescription;
+        msg = parentBlockDescription;
+    } else {
+        cb2 = cb;
+        msg = `${parentBlockDescription} - ${blockDescription}`;
+    }
+    const newTestBlock = (b, c) => exports.t(msg, b, c);
+    newTestBlock.test = exports.unitTest(tape, msg);
+    newTestBlock.skip = exports.unitTest(tape.skip, msg);
+    newTestBlock.only = exports.unitTest(tape.only, msg);
+    return cb2(newTestBlock);
+};
 
 /**
- * Returns a curried function that will host several tests block with a merged
- * description
- * @param {String} moduleDescription Module description
- * @param {String} blockDescription Test block description
- * @return {CurriedFunction} Test block function
+ * Identity function
+ * @param {Mixed} input - Value to be returned
+ * @return {Mixed} Same as input
  */
-exports.testModuleBlock = function testModuleBlock(moduleDescription, cb) {
-    return cb(
-        exports.testBlock(moduleDescription)
-    );
-}
+exports.idFn = function idFn(input) {
+    return input;
+};
 
 /**
  * Returns null
- * @return {Null}                   Null
+ * @return {Null} Null
  */
 exports.nullFn = function nullFn() {
     return null;
-}
+};
 
 /**
  * Returns a function that returns null
@@ -61,117 +74,162 @@ exports.nullFn = function nullFn() {
  */
 exports.nullFnHO = function nullFnHO() {
     return () => exports.nullFn();
-}
+};
 
 /**
  * Throws an Error
- * @param {String} error Error message
+ * @param {String} error - Error message
  * @return {Error} Error thrown
  */
 exports.throwFn = function throwFn(error) {
     throw new Error(error);
-}
+};
 
 /**
  * Returns a function that throws an Error
- * @param  {String} error Error message
+ * @param {String} error - Error message
  * @return {Function} Function that throws an error
  */
 exports.throwFnHO = function throwFnHO(error) {
     return () => exports.throwFn(error);
-}
+};
 
 /**
  * Returns a resolved Promise
- * @param  {String} value Resolved value
- * @return {Promise.Mixed} Promise resoved with given value
+ * @param {String} value - Resolved value
+ * @return {Promise<Any>} Promise resoved with given value
  */
 exports.resolveFn = function resolveFn(value) {
-    return Promise.resolve(value);
-}
+    return BPromise.resolve(value);
+};
 
 /**
  * Returns a function that return a resolved Promise with given value
- * @param  {String} value Resolved value
- * @return {Function} Function that return a resolved Promise with given value
+ * @param  {String} value - Resolved value
+ * @return {Function} Function that returns a resolved Promise with given value
  */
-//exports.resolveFnHO = function resolveFnHO(value) {
-    //return () => exports.resolveFn(value);
-//}
+exports.resolveFnHO = function resolveFnHO(value) {
+    return () => exports.resolveFn(value);
+};
 
 /**
  * Returns a rejected Promise
- * @param  {String} reason Rejection reason
- * @return {Promise.Mixed} Promise rejected with given reason
+ * @param  {String} reason - Rejection reason
+ * @return {Promise<Any>} Promise rejected with given reason
  */
-//exports.rejectFn = function rejectFn(reason) {
-    //return Promise.reject(reason);
-//}
+exports.rejectFn = function rejectFn(reason) {
+    return BPromise.reject(reason);
+};
 
 /**
  * Returns a function that return a rejected Promise with given reason
- * @param  {String} reason Rejection reason
+ * @param  {String} reason - Rejection reason
  * @return {Function} Function that return a rejected Promise with given reason
  */
 //exports.rejectFnHO = function rejectFnHO(reason) {
-    //return () => exports.rejectFn(reason);
-//}
-
-/**
- * Returns a function that throw a "Please stub this function" error
- * @param  {String} fnName Function name
- * @return {Function} Function that throws a "Please stub this function" error
- */
-exports.stubMe = function stubMe(fnName) {
-    return function stubThis() {
-        throw new Error("Please stub this function: " + fnName);
-    };
-}
+//return () => exports.rejectFn(reason);
+//};
 
 // Module names, based on project root, to ensure consistency across tests
-// @TODO Dynamically build these
-exports.path = "path";
-exports.lodash = "lodash";
-exports.tape = "tape";
-exports.proxyquire = "proxyquire";
-exports.express = "express";
-
 exports.config = path.resolve("src/config");
 exports.utils = path.resolve("src/lib/utils");
-exports.middlewares = path.resolve("src/lib/middlewares");
+exports.middlewareLoader = path.resolve("src/lib/middlewares");
 
 // Default stubs that will be used across tests
 // Any new dependency must be added here to be stubbed
-// @TODO Dynamically build these
 const defaultStubs = {
 
     // External dependencies
-    [exports.path]: exports.stubMe("path"),
-    [exports.lodash]: exports.stubMe("lodash"),
-    [exports.tape]: exports.stubMe("tape"),
-    [exports.proxyquire]: exports.stubMe("proxyquire"),
-    [exports.express]: exports.stubMe("express"),
-
-    // Internal dependencies
-    [exports.config]: {
-        getService: exports.stubMe("getService")
+    path: path,
+    lodash: lodash,
+    tape: {
+        '@noCallThru': true
     },
-    [exports.utils]: {
-        getLogger: exports.stubMe("getLogger")
-    },
-    [exports.middlewares]: {
-        start: exports.stubMe("start")
+    proxyquire: proxyquire,
+    express: {
+        '@noCallThru': true
     }
+};
+// Internal dependencies
+defaultStubs[exports.config] = {
+    '@noCallThru': true
+};
+defaultStubs[exports.utils] = {
+    '@noCallThru': true
+};
+defaultStubs[exports.middlewareLoader] = {
+    '@noCallThru': true
 };
 
 /**
  * Require a proxified module with defaultStubs eventually overloaded with
  * custom stubs
- * @param  {String} moduleName Proxified module name
- * @param  {Object} customStubs Custom stubs for the proxified module
- * @return {Object} Required proxified module
+ * @param  {String} moduleName - Proxified module name
+ * @param  {Object} customStubs - Custom stubs for the proxified module
+ * @return {Any} Required proxified module
  */
 exports.prepareStubs = lodash.curry(
     function prepareStubs(moduleName, customStubs) {
-        return proxyquire(moduleName, lodash.merge({}, defaultStubs, customStubs));
+        return proxyquire(moduleName, lodash.flow(
+            lodash.merge(exports.convertIntoComputedProperties(customStubs)),
+            lodash.merge(defaultStubs)
+        )({}));
     });
+
+
+// Default that will be used for factories
+// Any new dependency must be added here to be stubbed
+const defaultContext = {
+    utils: {
+        getLogger: exports.nullFnHO
+    }
+};
+
+/**
+ * Convert an object into another object with computed properties by using the given
+ * object properties names as variables declared in this module
+ * For example, given an object { a: 'foo', b: 'bar' }
+ * this function will return an object { [a]: 'foo', [b]: 'bar' } with a and b
+ * eventually defined as variables in this module.
+ * If they are not defined, this function will assume that they refer to node
+ * modules instead of local ones.
+ *
+ * @param  {Object} object - Input object
+ * @return {Object} Converted object
+ */
+exports.convertIntoComputedProperties = function convertIntoComputedProperties(object) {
+    const newObj = {};
+    const keys = lodash.keys(object);
+    lodash.map(function(property) {
+            if (exports[property]) {
+                newObj[exports[property]] = object[property];
+            } else {
+                newObj[property] = object[property];
+            }
+        },
+        keys
+    );
+    return newObj;
+};
+
+/**
+ * Require a module and create an instance with defaultStubs eventually overloaded with
+ * custom context
+ * @param  {String} moduleName - Module name
+ * @param  {Object} customContext - Custom context for the module factory
+ * @return {Object} Required module instance
+ */
+exports.prepareInstance = lodash.curry(
+    function prepareInstance(moduleName, customContext) {
+        return require(moduleName).factory(lodash.flow(
+            lodash.merge(customContext),
+            lodash.merge(defaultContext)
+        )({}));
+    });
+
+/**
+ * Stubbin and spying functions exports
+ * To abstract Sinon.js in tests
+ */
+exports.stub = sinon.stub;
+exports.spy = sinon.spy;
--- src/lib/test-helpers.spec.js
+++ src/lib/test-helpers.spec.js
@@ -3,37 +3,79 @@
 const path = require("path");
 
 const {
-    // Primary test functions
-    testModuleBlock,
+    // Test harness
+    t,
     prepareStubs,
 
     // Helpers
-    nullFn,
-
-    // Dependencies to stub
-    tape
+    nullFn
 } = require(path.resolve("src/lib/test-helpers"));
 
 const m = prepareStubs(path.resolve(__dirname, "./test-helpers"));
 
-testModuleBlock("Test helpers library", (testBlock) => {
-    testBlock("unitTest()", (unitTest) => {
-        unitTest("when given a unit test", (t) => {
+t("Test helpers library", function(t) {
+    t("unitTest()", function(t) {
+        t.test("when given a unit test", function(t) {
             t.equal(
-                m({
-                    [tape]: nullFn
-                }).unitTest('', '', nullFn),
-                null,
-                "should call Tape");
+                m({}).unitTest(() => true, '', '', nullFn),
+                true,
+                "should call its test engine");
             t.end();
         });
     });
-    testBlock("stubMe()", (unitTest) => {
-        unitTest("when given a unit test", (t) => {
+    t("idFn()", function(t) {
+        t.test("when given any input", function(t) {
+            t.equal(
+                m({}).idFn('foo'),
+                'foo',
+                "should return the same value");
+            t.end();
+        });
+    });
+    t("nullFn()", function(t) {
+        t.test("when given any input", function(t) {
+            t.equal(
+                m({}).nullFn(),
+                null,
+                "should return null");
+            t.end();
+        });
+    });
+    t("nullFnHO()", function(t) {
+        t.test("when given any input", function(t) {
+            t.equal(
+                m({}).nullFnHO()(),
+                null,
+                "should return a function that returns null");
+            t.end();
+        });
+    });
+    t("throwFn()", function(t) {
+        t.test("when given any input", function(t) {
             t.throws(
-                m({}).stubMe('a'),
+                m({}).throwFn,
+                "should throw an error");
+            t.end();
+        });
+    });
+    t("throwFnHO()", function(t) {
+        t.test("when given any input", function(t) {
+            t.throws(
+                m({}).throwFnHO(),
                 "should return a function that throws an error");
             t.end();
         });
+    });
+    t("resolveFn()", function(t) {
+        t.test("when given any input", (t) =>
+            m({}).resolveFn()
+            .then(() => t.pass("should return a resolved Promise"))
+        );
+    });
+    t("resolveFnHO()", function(t) {
+        t.test("when given any input", (t) =>
+            m({}).resolveFnHO()()
+            .then(() => t.pass("should return a function that returns a resolved Promise"))
+        );
     });
 });
