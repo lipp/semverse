dd7312a552fe7f73c2cb801f79101fa3cf5fde46
flaoua <stephane.trebel@gmail.com>
2016-06-02 18:00:59 +0200

    Update service main function to properly load middlewares

--- src/index.js
+++ src/index.js
@@ -3,36 +3,55 @@
 const path = require("path");
 const express = require("express");
 
+const BPromise = require("bluebird");
+
 const config = require(path.resolve(__dirname, "./config"));
 const utils = require(path.resolve(__dirname, "./lib/utils"));
-const middlewares = require(path.resolve(__dirname, "./lib/middlewares"));
+const middlewareLoader = require(path.resolve(__dirname, "./lib/middlewares"));
 
 /**
- * Service starter function
- *
- * @return {Promise}        Fulfilled when service is started
+ * @name instanciateService
+ * @description Instanciate and start Service
+ * @param {Object} context - Current context
+ * @return {Promise} Fulfilled when service is started
+ **/
+exports.instanciateService = function instanciateService(context) {
+    return BPromise
+        .try(function() {
+            const log = utils.getLogger();
+            log("info", "Instanciating service...");
+            const serviceInstance = middlewareLoader.factory(context);
+            log("info", "Service instanciated");
+            return serviceInstance;
+        });
+};
+
+/**
+ * @name main
+ * @description The holy function that will start everything
+ * @return {Promise} Fulfilled when service is started
  **/
 exports.main = function main() {
-    try {
-        const logInfo = utils.getLogger("info");
-        const logCritical = utils.getLogger(
-            "critical",
-            "A critical error happened while starting the service:"
-        );
-        logInfo("Starting service...", {});
-        const app = express();
-        const serviceConfig = config.getService();
-        return middlewares.start(app, serviceConfig, utils)
-            .then(() => logInfo("Service started", {}))
-            .catch(logCritical);
-    } catch (error) {
-        return Promise.reject(error);
-    }
+    return BPromise
+        .try(function() {
+            const log = utils.getLogger();
+            log("info", `Hi ! Welcome to Semverse !`);
+            log("info", `Please wait while I prepare everything :3`);
+            const context = {
+                utils: utils,
+                config: config
+            };
+            return exports.instanciateService(context);
+        })
+        .then((serviceInstance) => serviceInstance.startInstance(express()))
+        .catch(function(error) {
+            const log = utils.getLogger();
+            log("critical", `Fatal error: ${error.stack}`);
+            return BPromise.reject(error);
+        });
 };
 
 /* istanbul ignore if */
-if (process.env.NODE_ENV !== "test") {
-    exports.main()
-        .then(() => console.log(`Service terminated`))
-        .catch((error) => console.log(`Fatal error: ${error}`));
+if (process.env.NODE_ENV === "production") {
+    exports.main();
 }
