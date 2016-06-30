55026438e23443f99e7bc7f6cd6acabd07873cee
flaoua <stephane.trebel@gmail.com>
2016-06-30 18:01:18 +0200

    Refactor entity models to be a model factory

--- src/models/entity.js
+++ src/models/entity.js
@@ -7,44 +7,54 @@
  */
 "use strict";
 
-// Entities list
-const entities = [];
+exports.factory = function factory( /*sequelize*/ ) {
 
-/**
- * Create a new entity
- * @return {String} New entity
- */
-exports.create = function create() {
-    const newEntity = {
-        id: entities.length,
-        name: "newEntity"
+    const instance = {};
+
+    // Exported model name for model grouping
+    instance.name = 'entity';
+
+    // Entities list
+    const entities = [];
+
+    /**
+     * Create a new entity
+     * @return {String} New entity
+     */
+    instance.create = function create() {
+        const newEntity = {
+            id: entities.length,
+            name: "newEntity"
+        };
+        entities.push(newEntity);
+        return newEntity;
     };
-    entities.push(newEntity);
-    return newEntity;
-};
 
-/**
- * Delete an entity
- * @return {String} Deleted entity
- */
-exports.remove = function remove() {
-    const entity = entities.pop();
-    return entity;
-};
+    /**
+     * Delete an entity
+     * @return {String} Deleted entity
+     */
+    instance.remove = function remove() {
+        const entity = entities.pop();
+        return entity;
+    };
 
-/**
- * Retrieve an entity
- * @return {String} Retrieved entity
- */
-exports.get = function get() {
-    const entity = entities[0];
-    return entity;
-};
+    /**
+     * Retrieve an entity
+     * @return {String} Retrieved entity
+     */
+    instance.get = function get() {
+        const entity = entities[0];
+        return entity;
+    };
 
-/**
- * Retrieve all entities
- * @return {Array} Entity list
- */
-exports.getAll = function getAll() {
-    return entities;
+    /**
+     * Retrieve all entities
+     * @return {Array} Entity list
+     */
+    instance.getAll = function getAll() {
+        return entities;
+    };
+
+    return instance;
 };
